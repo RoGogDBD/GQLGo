3de8bb034cfac82e3ab67266510ee96b18a78019
Makar <makarkudryavtsev@mail.ru>
2026-02-06 17:51:27 +0300

    fix: испраление ENV переменной

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -39,7 +39,6 @@ func LoadFromEnv() Config {
 	if v := os.Getenv("USE_POSTGRES"); v != "" {
 		cfg.UsePostgres = v == "true" || v == "1" || v == "yes" || v == "y"
 	} else if v := os.Getenv("POSTGRES"); v != "" {
-		// Backward compatibility with older env name.
 		cfg.UsePostgres = v == "true" || v == "1" || v == "yes" || v == "y"
 	}
 
